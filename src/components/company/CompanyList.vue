7f80409931bc1114d57295c6a299a0390934490a
matan-ode <matanodentz@gmail.com>
2025-07-13 21:49:36 +0300

    Add basic CompanyPreview

--- src/components/company/CompanyList.vue
+++ src/components/company/CompanyList.vue
@@ -1,9 +1,14 @@
 <template>
-  <pre>{{ companies }}</pre>
+  <table class="company-list">
+      <tr v-for="company in companies" :key="company.id">
+        <CompanyPreview :company="company" />
+      </tr>
+  </table>
 </template>
 
 <script setup>
 import { defineProps } from 'vue'
+import CompanyPreview from './CompanyPreview.vue'
 
 defineProps({
   companies: Array
---
+++ src/components/company/CompanyPreview.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="company-preview">
+        <td>
+          {{ company.legalName }}
+          {{ company.country }}
+        </td>
+  </article>
+</template>
+
+<script setup>
+
+defineProps({
+  company: Object
+})
+
+</script>
+
+
+<!-- {
+  "id": "pAuC6RQ71bBG",
+  "active": true,
+  "name": "Market Data Insights LLC",
+  "legalName": "Market Data Insights LLC",
+  "country": "USA",
+  "dateAdded": "Sun, 26 Jan 2025 16:54:36 GMT",
+  "isDpfFound": false,
+  "parentId": "hDQkIp9PldZO",
+  "providesAiServices": true
+} -->
