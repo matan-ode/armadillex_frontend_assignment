66932f2fff15c4c6bb1b44176c3183be13a007d8
matan-ode <matanodentz@gmail.com>
2025-07-14 11:44:29 +0300

    Add create company prompt modal and aiGenerateNames methodology

--- src/components/company/CompanyList.vue
+++ src/components/company/CompanyList.vue
@@ -3,7 +3,7 @@
     <q-table @row-click="onRowClick" flat bordered title="Companies" :rows="rows" :columns="columns" row-key="id"
       :filter="filter" :loading="loading">
       <template v-slot:top>
-        <q-btn color="primary" :disable="loading" label="Add Company" @click="addCompany" />
+        <q-btn label="Add a Company" color="primary" @click="prompt = true" />
         <q-space />
         <q-input borderless dense debounce="300" color="primary" placeholder="Search" v-model="filter">
           <template v-slot:append>
@@ -13,6 +13,28 @@
       </template>
     </q-table>
   </div>
+
+  <div class="q-pa-md q-gutter-sm">
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Company Name:</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input dense v-model="aiName" autofocus @keyup.enter="prompt = false" />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <!-- <q-btn flat label="Add address" v-close-popup /> -->
+          <q-btn v-close-popup color="primary" :disable="loading" label="AI Generator" @click="aiGenerateNames" />
+
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+
 </template>
 
 <script setup>
@@ -46,11 +68,17 @@ const originalRows = props.companies
 const loading = ref(false)
 const filter = ref('')
 const rows = ref([...originalRows])
-
+const prompt = ref(false)
+const aiName = ref('')
 
 function onRowClick(ev, row) {
 
   console.log('clicked on', row, ev);
+
+}
+
+function aiGenerateNames(){
+  console.log('WORKSSS');
 
 }
 
